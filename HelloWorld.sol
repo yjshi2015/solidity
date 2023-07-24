623cbd22f66faa7379a08b7e8f5d8933c226c672
yjshi2015 <495054021@qq.com>
2023-07-24 20:04:37 +0800

    合约之间的相互调用

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -3,6 +3,13 @@ pragma solidity ^0.8.7;
 
 contract HelloWordl {
     string public myString = "hello world!";
-    bytes32 public myBytes;
     uint256 public myInt;
+
+    function convertToBytes32(string memory input) public pure returns (bytes32) {
+        bytes32 output = bytes32(0);
+        assembly {
+            output := mload(add(input, 32))
+        }
+        return output;
+    }
 }
---
+++ contract-callOtherContract.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.22 <0.9.0;
+
+/*
+只部署TokenCreator合约即可，通过该合约的createToken函数生成 OwnedToken合约的实例地址，
+然后就可以操作 OwnedToken合约实例的状态变量，此时的msg.sender即为TokenCreator合约。
+*/
+
+contract OwnedToken {
+    // `TokenCreator` is a contract type that is defined below.
+    // It is fine to reference it as long as it is not used
+    // to create a new contract.
+    TokenCreator public creator;
+    address public owner;
+    bytes32 public name;
+
+    event logAddr(address);
+
+    // This is the constructor which registers the
+    // creator and the assigned name.
+    constructor(bytes32 name_) {
+        emit logAddr(msg.sender);
+        // State variables are accessed via their name
+        // and not via e.g. `this.owner`. Functions can
+        // be accessed directly or through `this.f`,
+        // but the latter provides an external view
+        // to the function. Especially in the constructor,
+        // you should not access functions externally,
+        // because the function does not exist yet.
+        // See the next section for details.
+        owner = msg.sender;
+
+        // We perform an explicit type conversion from `address`
+        // to `TokenCreator` and assume that the type of
+        // the calling contract is `TokenCreator`, there is
+        // no real way to verify that.
+        // This does not create a new contract.
+        creator = TokenCreator(msg.sender);
+        name = name_;
+    }
+
+    function changeName(bytes32 newName) public {
+        emit logAddr(msg.sender);
+        emit logAddr(address(creator));
+        // Only the creator can alter the name.
+        // We compare the contract based on its
+        // address which can be retrieved by
+        // explicit conversion to address.
+        if (msg.sender == address(creator))
+            name = newName;
+    }
+
+    function transfer(address newOwner) public {
+        emit logAddr(msg.sender);
+        emit logAddr(address(creator));
+
+        // Only the current owner can transfer the token.
+        if (msg.sender != owner) return;
+
+        // We ask the creator contract if the transfer
+        // should proceed by using a function of the
+        // `TokenCreator` contract defined below. If
+        // the call fails (e.g. due to out-of-gas),
+        // the execution also fails here.
+        if (creator.isTokenTransferOK(owner, newOwner))
+            owner = newOwner;
+    }
+}
+
+
+contract TokenCreator {
+
+    // 0x73796a0000000000000000000000000000000000000000000000000000000000
+    function createToken(bytes32 name)
+        public
+        returns (OwnedToken tokenAddress)
+    {
+        // Create a new `Token` contract and return its address.
+        // From the JavaScript side, the return type
+        // of this function is `address`, as this is
+        // the closest type available in the ABI.
+        return new OwnedToken(name);
+    }
+
+    //
+    function changeName(OwnedToken tokenAddress, bytes32 name) public {
+        // Again, the external type of `tokenAddress` is
+        // simply `address`.
+        tokenAddress.changeName(name);
+    }
+
+    // Perform checks to determine if transferring a token to the
+    // `OwnedToken` contract should proceed
+    function isTokenTransferOK(address currentOwner, address newOwner)
+        public
+        pure
+        returns (bool ok)
+    {
+        // Check an arbitrary condition to see if transfer should proceed
+        return keccak256(abi.encodePacked(currentOwner, newOwner))[0] == 0x7f;
+    }
+}
