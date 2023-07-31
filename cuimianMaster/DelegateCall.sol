1f3f28e91ea2bf5b6a978a715c17b5567bca307f
yjshi2015 <495054021@qq.com>
2023-07-31 18:47:20 +0800

    add 委托调用，一种升级合约的新方式

--- cuimianMaster/DelegateCall.sol
+++ cuimianMaster/DelegateCall.sol
@@ -25,6 +25,37 @@ A  ---> B  ---> C
 */
 
 contract TestContract {
+    //被调用合约的结构布局必须和委托合约一致
+    //如果有新增的状态变量，要按照原变量顺序添加到最后
     uint public num;
     address public sender;
+    uint public value;
+
+    function setVars(uint _num) external payable {
+        num = _num;
+        sender = msg.sender;
+        value = msg.value;
+    }
+}
+
+contract DelegateCall {
+    uint public num;
+    address public sender;
+    uint public value;
+
+    function setVars(address _test, uint _num) external payable {
+        /*
+        委托调用方式1，容易发生拼写错误
+        _test.delegatecall(
+            abi.encodeWithSignature("setVars(uint256)", _num)
+        );
+        */
+
+        //委托调用方式2
+        (bool success, ) = _test.delegatecall(
+             abi.encodeWithSelector(TestContract.setVars.selector, _num)
+        );
+        
+        require(success, "delegatecall failed, wakaka~");
+    }
 }
